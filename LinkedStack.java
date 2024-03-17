3ac303b47ad736055767175ffa3e5581669ad1f7
JustTransScripts <157331180+JustTransScripts@users.noreply.github.com>
2024-03-17 16:38:30 -0700

    LinkedStack based on source code, rename old one to LinkedStackOld

--- LinkedStack.java
+++ LinkedStack.java
@@ -1,83 +1,90 @@
-// task 2 - LinkedStack class instance to implement the algorithm convertToPostfix
-import java.util.Stack;
+import java.util.EmptyStackException;
+/**
+    A class of stacks whose entries are stored in a chain of nodes.
+    @author Frank M. Carrano and Timothy M. Henry
+    @version 5.0
+*/
+public final class LinkedStack<T> implements StackInterface<T>
+{
+	private Node topNode; // References the first node in the chain
+  
+   public LinkedStack()
+   {
+      topNode = null;
+   } // end default constructor
+  
+   public void push(T newEntry)
+   {
+      Node newNode = new Node(newEntry, topNode);
+      topNode = newNode;
+   } // end push
 
-public class LinkedStack {
+   public T peek()
+   {
+       if (isEmpty())
+           throw new EmptyStackException();
+       else
+           return topNode.getData();
+   } // end peek
 
-    // Method to convert infix expression to postfix expression
-    public static String convertToPostfix(String infixExpression) {
+   public T pop()
+   {
+       if (isEmpty())
+       {
+           throw new EmptyStackException();
+       }
+       else
+       {
+         Node oldNode = topNode;
+         topNode = oldNode.getNextNode();
+         return oldNode.getData();
+       } // end if
+   } // end pop
 
-        // Stack to hold operators
-        Stack<Character> stack = new Stack<>();
+   public boolean isEmpty()
+   {
+       return topNode == null;
+   } // end isEmpty
+   
+   public void clear()
+   {
+       topNode = null;
+   } // end clear
 
-        // StringBuilder to store the postfix expression
-        StringBuilder postfixExpression = new StringBuilder();
-
-        // Iterate through each character 
-        for (int i = 0; i < infixExpression.length(); i++) {
-            char currentChar = infixExpression.charAt(i);
-
-            // If character is a letter or a digit, add it to postfix 
-            if (Character.isLetterOrDigit(currentChar)) {
-                postfixExpression.append(currentChar);
-            }
-            // If character is an opening parenthesis, push it onto the stack
-            else if (currentChar == '(') {
-                stack.push(currentChar);
-            }
-            // If character is a closing parenthesis
-            else if (currentChar == ')') {
-                // Pop operators from stack and append to postfix expression until an opening parenthesis is found
-                while (!stack.isEmpty() && stack.peek() != '(') {
-                    postfixExpression.append(stack.pop());
-                }
-                // Pop opening parenthesis from the stack
-                if (!stack.isEmpty()) {
-                    stack.pop();
-                }
-            }
-            // If character is an operator
-            else {
-                // Pop operators from the stack and append to the postfix expression 
-                while (!stack.isEmpty() && precedence(currentChar) <= precedence(stack.peek())) {
-                    postfixExpression.append(stack.pop());
-                }
-                // Push  current operator onto the stack
-                stack.push(currentChar);
-            }
-            System.out.println(postfixExpression);
-        }
-
-        // Pop any remaining operators from the stack and append to the postfix 
-        while (!stack.isEmpty()) {
-            postfixExpression.append(stack.pop());
-        }
-
-        // Return the postfix expression as a string
-        return postfixExpression.toString();
-    }
-
-    // Method to determine the precedence of an operator
-    private static int precedence(char operator) {
-        switch (operator) {
-            case '*':
-            case '/':
-                return 2;
-            case '+':
-            case '-':
-                return 1;
-            default:
-                return 0;
-        }
-    }
-    
-    // testing
-    public static void main(String[] args) {
-        // infix expression
-        String infixExpression = "a*b/(c-a)+d*e";
-        // infix to postfix
-        String postfixExpression = convertToPostfix(infixExpression);
-        // print 
-        System.out.println("Infix Expression: " + infixExpression);
-        System.out.println("Postfix Expression: " + postfixExpression);
-    }
-}
+	private class Node
+	{
+      private T    data; // Entry in stack
+      private Node next; // Link to next node
+      
+      private Node(T dataPortion)
+      {
+         this(dataPortion, null);
+      } // end constructor
+      
+      private Node(T dataPortion, Node linkPortion)
+      {
+         data = dataPortion;
+         next = linkPortion;
+      } // end constructor
+      
+      private T getData()
+      {
+         return data;
+      } // end getData
+      
+      private void setData(T newData)
+      {
+         data = newData;
+      } // end setData
+      
+      private Node getNextNode()
+      {
+         return next;
+      } // end getNextNode
+      
+      private void setNextNode(Node nextNode)
+      {
+         next = nextNode;
+      } // end setNextNode
+	} // end Node
+} // end LinkedStack
