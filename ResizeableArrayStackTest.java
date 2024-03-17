148b0bc863d1ad4d8c67e9263b5809487522247a
JustTransScripts <157331180+JustTransScripts@users.noreply.github.com>
2024-03-17 16:38:45 -0700

    minor tweaks in resizeable test

--- ResizeableArrayStackTest.java
+++ ResizeableArrayStackTest.java
@@ -1,26 +1,22 @@
 public class ResizeableArrayStackTest {
-    public class Main {
-        public static void main(String[] args) {
-            // Create an ArrayStack object
-            ResizeableArrayStack<Integer> stack = new ResizeableArrayStack<>();
+    public static void main(String[] args) {
+        // Create a ResizeableArrayStack object
+        ResizeableArrayStack<Integer> stack = new ResizeableArrayStack<>();
     
-            // Test cases
-            String postfix1 = "26+35-/";
-            String postfix2 = "234*5*-";
-            String postfix3 = "234-/5*";
-            String postfix4 = "6342^*+5-";
+        // Test cases
+        String postfix1 = "26+35-/";
+        String postfix2 = "234*5*-";
+        String postfix3 = "234-/5*";
+        String postfix4 = "6342^*+5-";
     
-            // Test postfix1: ae+bd-/
-            int result1 = stack.evaluatePostfix(postfix1);
-            int result2 = stack.evaluatePostfix(postfix2);
-            int result3 = stack.evaluatePostfix(postfix3);
-            int result4 = stack.evaluatePostfix(postfix4);
-            System.out.println("Result of " + postfix1 + " = " + result1);
-            System.out.println("Result of " + postfix2 + " = " + result2);
-            System.out.println("Result of " + postfix3 + " = " + result3);
-            System.out.println("Result of " + postfix4 + " = " + result4);
-
-        }
+        // Test postfix evaluation
+        int result1 = stack.evaluatePostfix(postfix1);
+        int result2 = stack.evaluatePostfix(postfix2);
+        int result3 = stack.evaluatePostfix(postfix3);
+        int result4 = stack.evaluatePostfix(postfix4);
+        System.out.println("Result of " + postfix1 + " = " + result1);
+        System.out.println("Result of " + postfix2 + " = " + result2);
+        System.out.println("Result of " + postfix3 + " = " + result3);
+        System.out.println("Result of " + postfix4 + " = " + result4);
     }
-    
 }
